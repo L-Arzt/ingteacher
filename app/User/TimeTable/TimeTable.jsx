b4f197be20cc631cd2ac025139d5e69196bef9b0
derarzt666. <smelov020@gmail.com>
2024-12-10 19:57:20 +0300

    2.0

--- app/User/TimeTable/TimeTable.jsx
+++ app/User/TimeTable/TimeTable.jsx
@@ -1,5 +1,5 @@
-'use client';
 
+'use client'
 import { useContext, useEffect, useRef, useState } from 'react';
 import {
     Table,
@@ -13,17 +13,15 @@ import Link from 'next/link';
 import { ThemeContext } from '../../components/ThemeProvider';
 import edit from '../../../public/edit.png';
 import Image from 'next/image';
-import MenuWeek from '@/app/components/MenuWeek';
-import nextImg from '../../../public/arrownexttable.png'
-import backImg from '../../../public/arrowbacktable.png'
+import { getSession } from 'next-auth/react';
 
-export default function TimeTableAdmin({ data, weekRange }) {
+export default function TimeTable({ data, weekRange }) {
     const [dataset, setDataset] = useState(data);
     const [hover, setHover] = useState({});
-    const [currentDay, setCurrentDay] = useState(new Date().getDay());
-    const [showWeek, setShowWeek] = useState(false);
     const context = useContext(ThemeContext);
     const [currentUser, setCurrentUser] = useState(null);
+
+    const slideContainerRef = useRef(null);
 
     useEffect(() => {
         async function fetchCurrentUser() {
@@ -71,47 +69,6 @@ export default function TimeTableAdmin({ data, weekRange }) {
         return result.toLocaleDateString('en-CA');
     }
 
-    const slideContainerRef = useRef(null);
-    const daysOnWeek = [
-        'Понедельник',
-        'Вторник',
-        'Среда',
-        'Четверг',
-        'Пятница',
-        'Суббота',
-        'Воскресенье'
-    ]
-    useEffect(() => {
-        if (context.weeks) {
-            async function getData() {
-                const resp = await fetch('/api/getTimeTableAdmin', {
-                    method: 'post',
-                    body: JSON.stringify({
-                        monday: context.weeks.from,
-                        sunday: context.weeks.to,
-                    }),
-                    headers: {
-                        'Content-Type': 'application/json'
-                    }
-                });
-
-                if (resp.ok) {
-                    const data = await resp.json();
-                    setDataset(data.data);
-                } else {
-                    console.error('Failed to fetch data');
-                }
-            }
-
-            getData();
-        }
-    }, [context.weeks]);
-
-    function getDateFromDay(date, day) {
-        var result = new Date(date);
-        result.setDate(result.getDate() + (day - 1));
-        return result.toLocaleDateString('en-CA');
-    }
 
     function buildTable(data) {
         const TimeLessonS = {
@@ -137,80 +94,72 @@ export default function TimeTableAdmin({ data, weekRange }) {
 
         for (let i = 1; i < 7; i++) {
             let tablePart = (
-                <TableRow className='transition-all duration-300 ease-in-out hover:bg-gray-100 shadow-sm rounded-md' key={`row-${i}`}>
-                    <TableCell key={`time-${i}`} className="py-3 px-5 font-medium text-gray-700">
-                        {TimeLessonS[i]}<hr />{TimeLessonPo[i]}
-                    </TableCell>
+                <TableRow className='customKletka' key={`row-${i}`}>
+                    <TableCell key={`time-${i}`}>{TimeLessonS[i]}<hr />{TimeLessonPo[i]}</TableCell>
                     {daysOfWeek.map((dayName, dayIndex) => {
                         const lesson = data.find(
                             (lesson) => lesson.weekDay === dayIndex + 1 && lesson.numberLesson === i
                         );
-
                         if (lesson) {
-                            if (lesson.userId === currentUser?.id) {
-                                // Current user is the one who booked this slot
+                            if (currentUser && lesson.userId === currentUser.id) {
                                 return (
-                                    <TableCell
-                                        className="relative py-3 px-5 border-t border-gray-300"
+                                    <TableCell className="relative border transform transition-transform duration-200"
                                         key={`lesson-${lesson.id}`}
                                         onMouseEnter={() => setHover(prevState => ({ ...prevState, [lesson.id]: true }))}
                                         onMouseLeave={() => setHover(prevState => ({ ...prevState, [lesson.id]: false }))}
                                     >
-                                        <div className="relative w-[140px] h-[70px] flex flex-col items-center justify-center transition-all duration-200">
+                                        <div className={`relative w-[140px] h-[70px] flex flex-col items-center justify-center transition-all duration-200`}>
                                             {hover[lesson.id] ? (
-                                                <div className='absolute z-20 flex items-center justify-center w-[250px] h-[120px] p-2 bg-white rounded-lg shadow-xl gap-2'>
+                                                <div className='absolute flex items-center justify-center  w-[250px] h-[120px] p-2 bg-gray-200 rounded-lg gap-2'>
                                                     <div className='flex-col'>
-                                                        <p className="font-semibold text-gray-800">{lesson.studentName}</p>
-                                                        <p className="text-gray-600">{lesson.description}</p>
+                                                        <p className="font-bold">{lesson.studentName}</p>
+                                                        <p>{lesson.description}</p>
                                                     </div>
                                                     <div>
                                                         <Link href={`/admin/book/UpdatePage/${lesson.id}`}>
-                                                            <button className="flex items-center justify-center bg-[#FF9100] w-[35px] h-[35px] rounded-md text-white">
-                                                                <Image className='w-8 h-8 m-1 bg-[#FF9100] rounded-lg p-1' src={edit} alt='editImg' />
+                                                            <button className="flex items-center justify-center bg-[#921CB0] w-[35px] h-[35px] rounded-md text-stone-50">
+                                                                <Image className='w-10 h-10 m-5 bg-[#921CB0] rounded-lg p-1' src={edit} alt='editImg' />
                                                             </button>
                                                         </Link>
                                                     </div>
                                                 </div>
                                             ) : (
                                                 <div className='absolute'>
-                                                    <p className="z-0 text-gray-600">{lesson.studentName.slice(0, 10)}...</p>
-                                                    <p className="z-0 text-gray-600">{lesson.description.slice(0, 10)}...</p>
+                                                    <p className="z-0">{lesson.studentName.slice(0, 10)}...</p>
+                                                    <p className="z-0">{lesson.description.slice(0, 10)}...</p>
                                                 </div>
                                             )}
                                         </div>
                                     </TableCell>
                                 );
                             } else {
-                                // Other user has booked the slot
                                 return (
-                                    <TableCell className="py-3 px-5 border-t border-gray-300" key={`occupied-${i}-${dayIndex}`}>
+                                    <TableCell className="border" key={`occupied-${i}-${dayIndex}`}>
                                         <div className='flex w-[140px] h-[70px] items-center justify-center flex-col gap-2'>
-                                            <h1 className='text-[#7E7E7E] font-medium'>Занято</h1>
+                                            <h1 className='text-[#7E7E7E]'>Занято</h1>
                                         </div>
                                     </TableCell>
                                 );
                             }
                         } else {
-                            // No lesson booked in this slot
                             return (
-                                <TableCell className="py-3 px-5 border-t border-gray-300" key={`empty-${i}-${dayIndex}`}>
+                                <TableCell className="border" key={`empty-${i}-${dayIndex}`}>
                                     <div className='flex w-[140px] h-[70px] items-center justify-center flex-col gap-2'>
-                                        <h1 className='text-[#7E7E7E] font-medium'>Свободно</h1>
+                                        <h1 className='text-[#7E7E7E]'>Свободно</h1>
                                         <Link
                                             href={`/Admin/book/${i}/${dayIndex + 1}/${getDateFromDay(
                                                 new Date(context?.weeks?.from),
                                                 dayIndex + 1
                                             )}`}
                                         >
-                                            <button className="bg-[#FF9100] hover:scale-105 active:scale-100 transition-all duration-200 text-white font-semibold py-2 px-4 rounded-md shadow-md">
-                                                Забронировать
-                                            </button>
+                                            <button className="flex items-center justify-center bg-[#921CB0] h-[30px] rounded-md text-stone-50 p-5">Забронировать</button>
                                         </Link>
                                     </div>
                                 </TableCell>
                             );
                         }
-                    })}
+                    }
+                    )}
                 </TableRow>
             );
             table.push(tablePart);
@@ -218,76 +167,21 @@ export default function TimeTableAdmin({ data, weekRange }) {
         return table;
     }
 
-
-
-    function handlePrevDay() {
-        setCurrentDay((prevDay) => (prevDay === 1 ? 7 : prevDay - 1));
-    }
-
-    function handleNextDay() {
-        setCurrentDay((prevDay) => (prevDay === 7 ? 1 : prevDay + 1));
-    }
-
-    function toggleShowWeek() {
-        setShowWeek((prevShowWeek) => !prevShowWeek);
-    }
-
-
     return (
-        <section className="relative flex items-center justify-center flex-col overflow-hidden w-full">
-            <div className="flex items-center justify-center gap-10 m-5">
-                <MenuWeek />
-                <button
-                    onClick={toggleShowWeek}
-                    className="flex items-center justify-center bg-[#FF910075] min-w-10 h-[30px] rounded-md text-stone-50 p-5 hover:bg-[#FF9100] transition-all"
-                >
-                    {showWeek ? 'Показать один день' : 'Показать всю неделю'}
-                </button>
-            </div>
-            <div className="absolute w-full ">
-                {showWeek ? (
-                    <></>
-                ) : (
-                    <div className="absolute flex justify-between w-full mt-4 top-[50%] px-10">
-                        <button onClick={handlePrevDay}>
-                            <Image src={backImg} alt="Previous Day" />
-                        </button>
-                        <button onClick={handleNextDay}>
-                            <Image src={nextImg} alt="Next Day" />
-                        </button>
-                    </div>
-                )}
-            </div>
+        <section className="flex items-center justify-center flex-col overflow-hidden">
             {dataset && (
-                <div className={`overflow-x-scroll ${!showWeek ? 'mr-20' : ''}`}>
+                <div className='overflow-x-scroll w-[100%] '>
                     <Table refProp={slideContainerRef}>
                         <TableHeader>
                             <TableRow>
-                                <TableHead className=" font-medium "></TableHead>
-                                {showWeek
-                                    ? daysOnWeek.map((day, index) => (
-                                        <TableHead
-                                            className=" font-semibold text-gray-500 text-sm text-center"
-                                            key={day}
-                                        >
-                                            {day}
-                                            <br />
-                                            <span className="text-sm text-gray-500">
-                                                {new Date(getDateFromDay(new Date(context?.weeks?.from), index + 1))
-                                                    .toLocaleDateString('ru-RU', { day: 'numeric', month: 'long' })}
-                                            </span>
-                                        </TableHead>
-                                    ))
-                                    : (
-                                        <TableHead className=" font-semibold text-gray-500 text-[18px] text-center">
-                                            {daysOnWeek[currentDay - 1]}
-                                            <br />
-                                            <span className="text-sm text-gray-500">
-                                                {new Date(getDateFromDay(new Date(context?.weeks?.from), currentDay))
-                                                    .toLocaleDateString('ru-RU', { day: 'numeric', month: 'long' })}
-                                            </span>
-                                        </TableHead>
-                                    )}
+                                <TableHead></TableHead>
+                                {['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс'].map((day, index) => (
+                                    <TableHead key={day}>
+                                        {day}
+                                        <br />
+                                        {getDateFromDay(new Date(context?.weeks?.from), index + 1)}
+                                    </TableHead>
+                                ))}
                             </TableRow>
                         </TableHeader>
                         <TableBody>{buildTable(dataset)}</TableBody>
